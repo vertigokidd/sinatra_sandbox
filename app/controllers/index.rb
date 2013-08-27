a4107d09158bad3f6e59ba4bfce85afbdd6bcbe8
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-27 10:31:14 -0500

    .

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,17 @@
 get '/' do
-  # Look in app/views/index.erb
+  puts "[LOG] Getting /"
+  puts "[LOG] Params: #{params.inspect}"
   erb :index
+end
+
+get '/cool_url' do
+  puts "[LOG] Getting /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :get_cool_url
+end
+
+post '/cool_url' do
+  puts "[LOG] Posting to /cool_url"
+  puts "[LOG] Params: #{params.inspect}"
+  erb :post_cool_url
 end
